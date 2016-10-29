d14cb53555a3ff5f630eb08f2b9abdf4964e93c9
Jari576 <jaribervoets@gmail.com>
2016-10-29 22:12:04 +0200

    added !kick functionality

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -2,7 +2,7 @@ package Client;
 
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class Client {
 
@@ -10,13 +10,18 @@ public class Client {
 	private static BufferedReader user_in;
 	private static BufferedReader server_in;
 	private static PrintWriter out; 
-
+	private static int port;
+	
 	public static void main(String[] args) {
 		socket = null;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("port:");
-		int port = sc.nextInt();
-		
+		try{
+			port = sc.nextInt();
+		} catch(InputMismatchException e) {
+			System.err.println("incorrect port input");
+			System.exit(1);
+		}
 		System.out.println("1. login");
 		System.out.println("2. new account");
 		sc.nextLine();
@@ -46,19 +51,25 @@ public class Client {
 					input = server_in.readLine();
 					if (input != null) {
 						System.out.println(input);
+						if(input.equals("ur kicked fgt")) {
+							socket.close();
+						}
 					}
 				}
-
+				
 				if (user_in.ready()) {
 					input = user_in.readLine();
 					out.println(input);
 				}
 			}
+			System.err.println("lost connection to the server");
 		sc.close();
 		} catch (IOException e) {
 			System.err.println("Couldn't connect to the server");
-			e.printStackTrace();
 			System.exit(1);
-		}
+		} catch(IllegalArgumentException e) {
+			System.err.println("incorrect port input");
+			System.exit(1);
+		} 
 	}
 }
